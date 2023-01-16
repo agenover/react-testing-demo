d65c2e96df7a41be6d6adc6b78d59789022ab464
agenover <agenover@findmypast.com>
2023-01-16 15:44:48 +0000

    Download react router and created basic routing for form and app

---
+++ src/form.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Form() {
+    return (
+      <main style={{ padding: "1rem 0" }}>
+        <Link to="/">Back to welcome page</Link>
+        <h2>Form</h2>
+      </main>
+    );
+  }
