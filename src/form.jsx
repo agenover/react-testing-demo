36397f39e4786a6b2a770c02d54552c5306f031f
agenover <agenover@findmypast.com>
2023-01-24 10:56:41 +0000

    Added checkboxes for preferred and private

--- src/form.jsx
+++ src/form.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { Formik, Field } from "formik";
+import { Formik } from "formik";
 import * as Yup from 'yup';
 
 export default function CoolForm() {
@@ -22,7 +22,7 @@ export default function CoolForm() {
           firstName: '',
           lastName: '',
           age: '',
-          gender: [],
+          gender: '',
           state: '',
           isPrivate: false,
           isPreferred: false,
@@ -40,9 +40,9 @@ export default function CoolForm() {
           age: Yup.number()
             .required("Ages is required")
             .positive()
-            .integer('Age must be a number between 1 and 110'),
-          gender: Yup.string()
-            .required("Gender is required"),
+            .min(1, "Minimum age 1")
+            .max(110, "Maximum age 110"),
+          isPreferred: Yup.boolean()
         })}
         onSubmit={(values) => (
           console.log(values)
@@ -50,68 +50,63 @@ export default function CoolForm() {
       >
         {formik => (
           <form onSubmit={formik.handleSubmit} className='container'>
-          <div className="container-fluid py-2">
-            <label htmlFor="firstName"> First Name </label>
-            <input id="" type='text' {...formik.getFieldProps('firstName')} />
-              {formik.touched.firstName && formik.errors.firstName ? (
-                <div>{formik.errors.firstName}</div>
-              ) : null}
-          </div>
-            
-          <div className="container-fluid py-2">
-            <label htmlFor="lastName"> Last Name </label>
-            <input id="" type='text'  {...formik.getFieldProps('lastName')} />
-              {formik.touched.lastName && formik.errors.lastName ? (
-                <div>{formik.errors.lastName}</div>
-              ) : null}
-          </div>
-            
-          <div className="container-fluid py-2">
-            <label htmlFor="age"> Age </label>
-            <input id="" type='text' {...formik.getFieldProps('age')} />
-              {formik.touched.age && formik.errors.age ? (
-                <div>{formik.errors.age}</div>
-              ) : null}
-          </div>
-
-          <div className="container-fluid py-2">
-            <div htmlFor="gender"> Gender
-              <label>
-                <Field type="radio" name="gender" value="male" />
-                Male
-              </label>
-              <label>
-                <Field type="radio" name="gender" value="female" />
-                Female
-              </label>
-              <label>
-                <Field type="radio" name="gender" value="other" />
-                Other
-              </label>
+            <div className="container-fluid">
+              <label htmlFor="firstName" className="p-2"> First Name </label>
+              <input id="" type='text' {...formik.getFieldProps('firstName')} />
+                {formik.touched.firstName && formik.errors.firstName ? (
+                  <div>{formik.errors.firstName}</div>
+                ) : null}
             </div>
-            <input id="" type='text' {...formik.getFieldProps('gender')} />
-              {formik.touched.gender && formik.errors.gender ? (
-                <div>{formik.errors.gender}</div>
-              ) : null}
+              
+            <div className="container-fluid">
+              <label htmlFor="lastName" className="p-2"> Last Name </label>
+              <input id="" type='text'  {...formik.getFieldProps('lastName')} />
+                {formik.touched.lastName && formik.errors.lastName ? (
+                  <div>{formik.errors.lastName}</div>
+                ) : null}
+            </div>
+              
+            <div className="container-fluid">
+              <label htmlFor="age" className="p-2"> Age </label>
+              <input id="" type='text' {...formik.getFieldProps('age')} />
+                {formik.touched.age && formik.errors.age ? (
+                  <div>{formik.errors.age}</div>
+                ) : null}
+            </div>
+
+            <div className="container-fluid">
+              <div htmlFor="gender" className="p-2"> Gender
+                <label className="p-2">Male</label>
+                <input type="radio" name="gender" value="male" />
+                <label className="p-2">Female</label>
+                <input type="radio" name="gender" value="female" />
+                <label className="p-2">Other</label>
+                <input type="radio" name="gender" value="other" />
               </div>
-
-              <div className="container-fluid py-2">
-              <label htmlFor="state"> State </label>
-              <input id="" type='text' />
             </div>
 
-            <div className="container-fluid py-2">
-            <label htmlFor="isPrivate"> Should profile be private </label>
-              <input id="" type='checkbox' />
+            <div className="container-fluid">
+              <label htmlFor="state" className="p-2"> State </label>
+                <input type="radio" name="state" value="living" />
+                <label className="p-2">Living</label>
+                <input type="radio" name="state" value="deceased" />
+                <label className="p-2">Deceased</label>
             </div>
 
-            <div className="container-fluid py-2">
-              <label htmlFor="isPreferred"> Is this the preferred profile </label>
-              <input id="" type='checkbox' />
+            <div className="container-fluid">
+              <label htmlFor="isPrivate" className="p-2"> Should profile be private </label>
+              <input id="" type='checkbox' {...formik.getFieldProps('isPrivate')}/>
+              <div>{formik.errors.isPrivate}</div>
             </div>
 
-            <div className="container-fluid py-2">
-              <label htmlFor="description"> Add a description (optional) </label>
+            <div className="container-fluid">
+              <label htmlFor="isPreferred" className="p-2"> Is this the preferred profile </label>
+              <input id="" type='checkbox' {...formik.getFieldProps('isPreferred')} />
+              <div>{formik.isPreferred}</div>
+            </div>
+
+            <div className="container-fluid">
+              <label htmlFor="description" className="p-2"> Add a description (optional) </label>
               <input id="" type='text' {...formik.getFieldProps('description')} />
             </div>
 
